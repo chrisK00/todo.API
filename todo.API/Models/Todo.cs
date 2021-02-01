2ab071f1c9eca020a4bb1953e27197b0a32e7a15
Christian Khalil <christian.khalil2000@gmail.com>
2021-02-01 22:28:05 +0100

    guid for id

--- todo.API/Models/Todo.cs
+++ todo.API/Models/Todo.cs
@@ -1,9 +1,15 @@
-﻿namespace todo.API.Models
+﻿using System;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+
+namespace todo.API.Models
 {
     public class Todo
     {
         //Should be a guid but for testing purposes leaving it as an int
-        public int Id { get; set; }
+        [DatabaseGenerated(DatabaseGeneratedOption.Identity)]
+        [Key]
+        public Guid Id { get; set; }
         public string Title { get; set; }
         public string Description { get; set; }
         public bool Completed { get; set; }
