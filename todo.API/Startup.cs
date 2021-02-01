f4181bd126c2530c4960ba32cb91a6a5856c54c2
Christian Khalil <christian.khalil2000@gmail.com>
2021-02-01 18:33:44 +0100

    init

--- todo.API/Startup.cs
+++ todo.API/Startup.cs
@@ -27,7 +27,7 @@ namespace todo.API
             services.AddCors();
         }
 
-        //  This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
+        //   This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
         public void Configure(IApplicationBuilder app, IWebHostEnvironment env)
         {
             if (env.IsDevelopment())
