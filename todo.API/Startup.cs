bba5aaf458c0284602dc2dacc09f24585539ee3e
Christian Khalil <christian.khalil2000@gmail.com>
2021-02-17 15:09:21 +0100

    setup

--- todo.API/Startup.cs
+++ todo.API/Startup.cs
@@ -50,8 +50,8 @@ namespace todo.API
         {
             if (env.IsDevelopment())
             {
-                app.UseMiddleware<ErrorHandlerMiddleware>();
-               // app.UseDeveloperExceptionPage();
+                // app.UseMiddleware<ErrorHandlerMiddleware>();
+                app.UseDeveloperExceptionPage();
                 app.UseSwagger();
                 app.UseSwaggerUI(c => c.SwaggerEndpoint("/swagger/v1/swagger.json", "todo.API v1"));
             }
