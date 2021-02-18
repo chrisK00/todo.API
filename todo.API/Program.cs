f5c097874b4c30e617535d8a030bbe7cfffbf9b2
chris <christianhunter707@gmail.com>
2021-02-18 18:33:30 +0100

    data seed

--- todo.API/Program.cs
+++ todo.API/Program.cs
@@ -1,12 +1,16 @@
 using Microsoft.AspNetCore.Hosting;
+using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Hosting;
 using Serilog;
+using System;
+using System.Linq;
+using todo.Data;
 
 namespace todo.API
 {
     public class Program
     {
-        public static void Main(string[] args)
+        public static async System.Threading.Tasks.Task Main(string[] args)
         {
             Log.Logger = new LoggerConfiguration()
                 .MinimumLevel.Debug()
@@ -14,7 +18,24 @@ namespace todo.API
                 .WriteTo.Console()
                 .CreateLogger();
 
-            CreateHostBuilder(args).Build().Run();
+            var host = CreateHostBuilder(args).Build();
+
+            using var scope = host.Services.CreateScope();
+            var services = scope.ServiceProvider;
+            try
+            {
+                var context = services.GetRequiredService<DataContext>();
+                if (context.Todos.Any())
+                {
+                    Seed.SeedTodos(context);
+                }
+            }
+            catch (Exception error)
+            {
+                var logger = services.GetRequiredService<ILogger>();
+                logger.Error(error.Message, "Seeding Error");
+            }
+            host.Run();
         }
 
         public static IHostBuilder CreateHostBuilder(string[] args) =>
